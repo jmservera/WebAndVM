778de0b8cfe2214d9847073a78a46138a035c092
Juan Manuel Servera <jmservera@outlook.com>
2016-05-03 12:10:26 +0200

    finish script

--- createpointtosite.ps1
+++ createpointtosite.ps1
@@ -7,14 +7,14 @@ $Text = Get-Content -Path "publicroot.cer"
 $CertificateText = for ($i=1; $i -lt $Text.Length -1 ; $i++){$Text[$i]}
 $CertificateText=($CertificateText | Out-String)
 $resourceGroup="webandvm"
-$vnetname="webandvm"
+$vnetname=$resourceGroup
 $defaultSubnetName="default"
 $gatewayName="pointtosite"
-$gatewaySubnetName=$gatewayName+"Subnet"
+$gatewaySubnetName="GatewaySubnet"# $gatewayName+"Subnet"
 $GWIPName=$gatewayName+"Ip"
 $GWIPconfName=$GWIPName+"Conf"
 $VPNClientAddressPool="172.16.201.0/24"
-$GWSubPrefix = "192.168.200.0/26"
+$GWSubPrefix = "10.0.2.0/24"
 
 Login-AzureRmAccount
 Select-AzureRmSubscription -SubscriptionId $subscriptionId
@@ -23,10 +23,15 @@ $location= (Get-AzureRmResourceGroup -Name $resourceGroup).Location
 
 $vnet=Get-AzureRmVirtualNetwork -ResourceGroupName $resourceGroup -name $vnetname
 $defaultSubnet=Get-AzureRmVirtualNetworkSubnetConfig -VirtualNetwork $vnet -Name $defaultSubnetName
-$gwsub=Add-AzureRmVirtualNetworkSubnetConfig -VirtualNetwork $vnet -Name $gatewaySubnetName -AddressPrefix $GWSubPrefix -Verbose
+Add-AzureRmVirtualNetworkSubnetConfig -Name $gatewaySubnetName -VirtualNetwork $vnet  -AddressPrefix $GWSubPrefix
+#must apply the change
+Set-AzureRmVirtualNetwork -VirtualNetwork $vnet
+$vnet=Get-AzureRmVirtualNetwork -ResourceGroupName $resourceGroup -name $vnetname
+$gwsub=Get-AzureRmVirtualNetworkSubnetConfig -Name $gatewaySubnetName -VirtualNetwork $vnet
 
 $pip = New-AzureRmPublicIpAddress -Name $GWIPName -ResourceGroupName $resourceGroup -Location $location -AllocationMethod Dynamic$ipconf = New-AzureRmVirtualNetworkGatewayIpConfig -Name $GWIPconfName -SubnetId $gwsub.Id -PublicIpAddressId $pip.Id
 
+
 $p2srootcert = New-AzureRmVpnClientRootCertificate -Name $rootcertname -PublicCertData $CertificateText
-$gateway= New-AzureRmVirtualNetworkGateway -Name $gatewayName -ResourceGroupName $resourceGroup -Location $location -IpConfigurations $ipconf -GatewayType Vpn -VpnType RouteBased -EnableBgp $false -GatewaySku Standard -VpnClientAddressPool $VPNClientAddressPool -VpnClientRootCertificates $p2srootcert
+New-AzureRmVirtualNetworkGateway -Name $gatewayName -ResourceGroupName $resourceGroup -Location $location -IpConfigurations $ipconf -GatewayType Vpn -VpnType RouteBased -EnableBgp $false -GatewaySku Basic -VpnClientAddressPool $VPNClientAddressPool -VpnClientRootCertificates $p2srootcert
 
